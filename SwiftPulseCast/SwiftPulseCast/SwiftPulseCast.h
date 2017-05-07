4823ca5f9fc16cb868fbfeb4fa81db3020a5a36e
Jeremiah Poisson <ffxfiend@igzactly.com>
2017-05-07 13:59:41 -0400

    Add SwiftPulseCast empty framework project

---
+++ SwiftPulseCast/SwiftPulseCast/SwiftPulseCast.h
@@ -0,0 +1,19 @@
+//
+//  SwiftPulseCast.h
+//  SwiftPulseCast
+//
+//  Created by Jeremiah Poisson on 5/7/17.
+//  Copyright © 2017 Jeremiah Poisson. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+//! Project version number for SwiftPulseCast.
+FOUNDATION_EXPORT double SwiftPulseCastVersionNumber;
+
+//! Project version string for SwiftPulseCast.
+FOUNDATION_EXPORT const unsigned char SwiftPulseCastVersionString[];
+
+// In this header, you should import all the public headers of your framework using statements like #import <SwiftPulseCast/PublicHeader.h>
+
+
