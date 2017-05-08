a00526d53dbacc7005279e6ad9fcb2be538fa355
Jeremiah Poisson <ffxfiend@igzactly.com>
2017-05-07 22:06:21 -0400

    INitial PulseNetworkError enum

---
+++ SwiftPulseCast/SwiftPulseCast/PulseError.swift
@@ -0,0 +1,13 @@
+//
+//  PulseError.swift
+//  SwiftPulseCast
+//
+//  Created by Jeremiah Poisson on 5/7/17.
+//  Copyright © 2017 Jeremiah Poisson. All rights reserved.
+//
+
+import Foundation
+
+public enum PulseNetworkError : Error {
+	case invalidParameters
+}
