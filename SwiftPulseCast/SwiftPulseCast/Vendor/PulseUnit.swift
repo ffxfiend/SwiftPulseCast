6a886dc1bc68a41572c26738be5f9acf8981594e
Jeremiah Poisson <ffxfiend@igzactly.com>
2017-05-08 20:23:57 -0400

    Add unit enum

---
+++ SwiftPulseCast/SwiftPulseCast/Vendor/PulseUnit.swift
@@ -0,0 +1,14 @@
+//
+//  PulseUnit.swift
+//  SwiftPulseCast
+//
+//  Created by Jeremiah Poisson on 5/8/17.
+//  Copyright © 2017 Jeremiah Poisson. All rights reserved.
+//
+
+import Foundation
+
+public enum PulseUnit : String {
+	case metric = "Metric"
+	case english = "English"
+}
